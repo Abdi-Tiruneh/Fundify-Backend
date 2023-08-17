e88e9012a79c55b6a212d02e7dae73ed53e0b07b
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-08-17 11:53:46 +0300

    add campaign routes to startup

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,11 +3,13 @@ import errorHandler from "../middleware/errorHandler";
 import users from "../routers/userRoutes";
 import auth from "../routers/authRoutes";
 import campaignTypes from "../routers/campaignTypeRoutes";
+import campaigns from "../routers/campaignRoutes";
 
 export default function setupRoutes(app: Application): void {
   app.use(express.json());
   app.use("/api/auth", auth);
   app.use("/api/users", users);
   app.use("/api/campaignTypes", campaignTypes);
+  app.use("/api/campaigns", campaigns);
   app.use(errorHandler);
 }
