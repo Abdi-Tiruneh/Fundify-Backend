f0770350945e20e2ad063184e3e32a0c4ff0f061
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-08-17 11:52:11 +0300

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import logging from "./config/logging";
 import jwtChecker from "./startup/jwtChecker";
 import routes from "./startup/routes";
 import { connectToDatabase } from "./config/database";
+import { initializeCloudinary } from "./config/cloudinary";
 
 dotenv.config();
 
 const app = express();
 
 logging();
+initializeCloudinary();
 jwtChecker();
 routes(app);
 connectToDatabase();
