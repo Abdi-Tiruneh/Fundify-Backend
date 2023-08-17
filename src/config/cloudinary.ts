9808469cac4e085b87b75fcae5e9d2bdaac47f40
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-08-17 11:48:14 +0300

    add cloudinary and multer to upload files

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,18 @@
+import { v2 as cloudinaryV2 } from "cloudinary";
+
+export function initializeCloudinary() {
+  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+    process.env;
+
+  if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
+    throw new Error("Cloudinary credentials are missing");
+  }
+
+  cloudinaryV2.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+  });
+}
+
+export default cloudinaryV2;
